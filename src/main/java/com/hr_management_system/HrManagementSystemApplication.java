1fd6110eb357d212d245f258c8a1a9087bc09e3f
Cristian Mitchell Harders López <cristianharders@gmail.com>
2025-12-17 11:34:39 +0100

    first commit

---
+++ src/main/java/com/hr_management_system/HrManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.hr_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrManagementSystemApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hellooooo are we working??");
+		SpringApplication.run(HrManagementSystemApplication.class, args);
+	}
+
+}
