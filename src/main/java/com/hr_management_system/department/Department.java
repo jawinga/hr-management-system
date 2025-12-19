c4f3da1953cb1ffd9d5eebd99d90733e076df809
Cristian Mitchell Harders López <cristianharders@gmail.com>
2025-12-19 14:21:18 +0100

    Department Entity created

---
+++ src/main/java/com/hr_management_system/department/Department.java
@@ -0,0 +1,32 @@
+package com.hr_management_system.department;
+
+import com.hr_management_system.employee.Employee;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    private DepartmentEnum departmentName;
+    private boolean active;
+    @Email
+    private String contactDept;
+    @ManyToOne
+    private Employee head;
+    @ManyToMany
+    private List<Employee> employees;
+    public String getDepartmentDescription() {
+        return departmentName != null ? departmentName.getDescription() : null;
+    }
+
+}
---
+++ src/main/java/com/hr_management_system/department/DepartmentEnum.java
@@ -0,0 +1,22 @@
+package com.hr_management_system.department;
+
+import lombok.Getter;
+
+@Getter
+public enum DepartmentEnum {
+    HR("Handles recruitment, employee relations, and payroll."),
+    FINANCE("Manages budgeting, financial planning, and audits."),
+    ENGINEERING("Responsible for software development and technical infrastructure."),
+    MARKETING("Focuses on branding, advertising, and market research."),
+    SALES("Responsible for generating revenue and client acquisition."),
+    CUSTOMER_SUPPORT("Assists clients with technical issues and inquiries."),
+    LEGAL("Handles contracts, compliance, and regulatory matters."),
+    OPERATIONS("Manages day-to-day business processes and efficiency.");
+
+    private final String description;
+
+    DepartmentEnum(String description) {
+        this.description = description;
+    }
+
+}
