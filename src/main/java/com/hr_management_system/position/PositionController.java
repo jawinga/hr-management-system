7845b8d15d1f9ff0f9a300a855647b98d3a98b3f
Cristian Mitchell Harders López <cristianharders@gmail.com>
2025-12-21 14:05:55 +0100

    Added POST method to Positions

--- src/main/java/com/hr_management_system/position/PositionController.java
+++ src/main/java/com/hr_management_system/position/PositionController.java
@@ -1,4 +1,34 @@
 package com.hr_management_system.position;
 
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/positions")
 public class PositionController {
+
+    private final PositionService positionService;
+
+    public PositionController(PositionService positionService) {
+        this.positionService = positionService;
+    }
+
+
+
+    @PostMapping
+    public ResponseEntity<Position> addPosition(@Valid @RequestBody Position p){
+
+        Position position = positionService.createPosition(p);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(position);
+
+    }
+
 }
