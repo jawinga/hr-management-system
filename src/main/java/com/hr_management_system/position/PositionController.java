b1dc3d5c28d29570adfc2b910de1e6813960022f
Cristian Mitchell Harders López <cristianharders@gmail.com>
2025-12-21 14:24:49 +0100

    completed Position controller

--- src/main/java/com/hr_management_system/position/PositionController.java
+++ src/main/java/com/hr_management_system/position/PositionController.java
@@ -1,14 +1,17 @@
 package com.hr_management_system.position;
 
 
+import com.hr_management_system.employee.Employee;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/positions")
@@ -21,6 +24,32 @@ public class PositionController {
     }
 
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Position> getPosition(@PathVariable Long id){
+
+
+        try{
+            Position position = positionService.findPosition(id);
+            return ResponseEntity.status(HttpStatus.OK).body(position);
+
+        }catch (EntityNotFoundException e){
+            Map<String, String> errorResponse = new HashMap<>();
+            errorResponse.put("error", e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body((Position) errorResponse);
+
+    }}
+
+
+    @GetMapping
+    public ResponseEntity<List<Position>> getAllEmployees() {
+
+        List<Position> list = positionService.findAllPositions();
+        return ResponseEntity.ok(list);
+        
+    }
+
+
+
 
     @PostMapping
     public ResponseEntity<Position> addPosition(@Valid @RequestBody Position p){
@@ -30,5 +59,38 @@ public class PositionController {
         return ResponseEntity.status(HttpStatus.CREATED).body(position);
 
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Position> updatePosition(@PathVariable("id")Long id, @Valid @RequestBody Position p){
+
+        if(!p.getId().equals(id)){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(e);
+        }
+
+        Position position = positionService.updatePosition(p);
+
+        return ResponseEntity.status(HttpStatus.OK).body(position);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePosition(@PathVariable("id")Long id){
+
+        try{
+
+            positionService.deletePosition(id);
+            return ResponseEntity.noContent().build();
+
+
+        }catch (EntityNotFoundException e){
+            return ResponseEntity.notFound().build();
+
+
+        }
+
+
+    }
+
+
 
 }
